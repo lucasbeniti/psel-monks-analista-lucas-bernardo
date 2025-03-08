a1e296e52b745e5d25f2765389ec9cef5ac43f52
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-03-08 15:12:13 -0300

    refactor: Cards on ProductsSection for small screens

--- src/components/ProductsSection/Card.tsx
+++ src/components/ProductsSection/Card.tsx
@@ -1,10 +1,10 @@
 const Card = () => {
   return (
-    <div className="rounded-lg bg-white w-fit pb-4 pt-[6px] px-[6px]">
+    <div className="w-full rounded-lg bg-white px-[6px] pt-[6px] pb-4">
       <img src="/image (1).png" alt="" className="w-full" />
       <div className="flex flex-col gap-2 p-2">
         <h1 className="text-2xl/[30px] font-semibold">Lorem ipsum dolor</h1>
-        <p className="text-xl font-normal ">
+        <p className="text-xl font-normal">
           Lorem ipsum dolor sit amet consectetur.
         </p>
       </div>
--- src/components/ProductsSection/Cards.tsx
+++ src/components/ProductsSection/Cards.tsx
@@ -2,7 +2,7 @@ import ProductCard from "./Card";
 
 const Cards = () => {
   return (
-    <div className="mt-6 mb-8 flex w-full flex-wrap items-center gap-4 sm:mt-10 sm:gap-6">
+    <div className="mt-6 mb-8 flex w-full flex-wrap items-center gap-4 sm:mt-10 sm:gap-6 xl:flex-nowrap">
       <ProductCard />
       <ProductCard />
       <ProductCard />
