ecfdd35cdda651fb81ae2d3715dabe902c6f5e99
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2025-03-08 12:28:03 -0300

    refactor: add ItemProps

--- src/components/CategorySession/Item.tsx
+++ src/components/CategorySession/Item.tsx
@@ -1,8 +1,12 @@
-const Item = () => {
+interface ItemProps {
+  text: string;
+}
+
+const Item = ({ text }: ItemProps) => {
   return (
-    <div className="w-[187px] h-[46px]">
+    <div className="w-[223px] h-[46px]">
       <p className="rounded-[32px] border border-[#7D26C9] py-2 px-10 bg-[#DFDCD5] text-center text-[#7D26C9] text-xl/[30px] font-semibold">
-        Perfumaria
+        {text}
       </p>
     </div>
   );
--- src/components/CategorySession/Items.tsx
+++ src/components/CategorySession/Items.tsx
@@ -2,22 +2,22 @@ import Item from "./Item";
 
 const Items = () => {
   return (
-    <div className="w-full h-full flex gap-6 flex-wrap">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+    <div className="w-full h-full flex gap-6 flex-wrap justify-center items-center">
+      <Item text="Perfumaria" />
+      <Item text="Corpo e banho" />
+      <Item text="Hidratante" />
+      <Item text="Desodorante" />
+      <Item text="Cabelos" />
+      <Item text="Maquiagem" />
+      <Item text="Rosto" />
+      <Item text="Casa" />
+      <Item text="Infantil" />
+      <Item text="Shampoo" />
+      <Item text="Sabonete" />
+      <Item text="Body splash" />
+      <Item text="Óleo corporal" />
+      <Item text="Corretivo" />
+      <Item text="Proteção solar" />
     </div>
   );
 };
